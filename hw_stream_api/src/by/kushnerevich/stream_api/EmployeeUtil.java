45f35bcb3da1f4c708b23b024bba2f2c99aa9455
Mabonma <alexey20010224@gmail.com>
2021-08-21 15:17:05 +0300

    rewrite methods at EmployeeUtil to stream API

--- hw_stream_api/src/by/kushnerevich/stream_api/EmployeeUtil.java
+++ hw_stream_api/src/by/kushnerevich/stream_api/EmployeeUtil.java
@@ -6,62 +6,78 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EmployeeUtil {
 
     public static Map<String, List<Employee>> groupByJobTitle(List<Employee> employeeList) {
-        Map<String, List<Employee>> resultMap = new HashMap<>();
-        for (int i = 0; i < employeeList.size(); i++) {
-            Employee employee = employeeList.get(i);
-            List<Employee> employeeSubList = resultMap
-                    .getOrDefault(employee.getJobTitle(), new ArrayList<>());
-            employeeSubList.add(employee);
-            resultMap.put(employee.getJobTitle(), employeeSubList);
-        }
-        return resultMap;
+
+//        Map<String, List<Employee>> resultMap = employeeList.stream().collect(Collectors.groupingBy(Employee::getJobTitle));
+//        for (int i = 0; i < employeeList.size(); i++) {
+//            Employee employee = employeeList.get(i);
+//            List<Employee> employeeSubList = resultMap
+//                    .getOrDefault(employee.getJobTitle(), new ArrayList<>());
+//            employeeSubList.add(employee);
+//            resultMap.put(employee.getJobTitle(), employeeSubList);
+//        }
+        return employeeList.stream().collect(Collectors.groupingBy(Employee::getJobTitle));
     }
 
     public static double calculateAverageSalary(List<Employee> employeeList) {
-        int sum = 0;
-        int count = 0;
-        for (Employee employee : employeeList) {
-            sum += employee.getSalary();
-            count++;
-        }
-        return (double) sum / count;
+
+//        return employeeList.stream().reduce(0.0,(x,y)-> x+y.getSalary(), (x, y)->x+y);
+        return employeeList.stream().mapToDouble(Employee::getSalary).average().orElseThrow();
+
+//        int sum = 0;
+//        int count = 0;
+//        for (Employee employee : employeeList) {
+//            sum += employee.getSalary();
+//            count++;
+//        }
+//        return (double) sum / count;
     }
 
     public static List<Employee> filterByName(List<Employee> employeeList, String name) {
-        List<Employee> resultList = new ArrayList<>();
-        for (var item : employeeList) {
-            if (item.getName().equals(name)) {
-                resultList.add(item);
-            }
-        }
-        return resultList;
+        return employeeList.stream().filter(x->x.getName().equals(name)).toList();
+
+//        List<Employee> resultList = new ArrayList<>();
+//        for (var item : employeeList) {
+//            if (item.getName().equals(name)) {
+//                resultList.add(item);
+//            }
+//        }
+//        return resultList;
     }
 
     public static List<Employee> filterByNameAndSortBySalaryDesc(List<Employee> employeeList,
                                                                  String name) {
-        List<Employee> resultList = new ArrayList<>();
-        for (var item : employeeList) {
-            if (item.getName().equals(name)) {
-                resultList.add(item);
-            }
-        }
-        resultList.sort(Comparator.comparing(Employee::getSalary).reversed());
-        return resultList;
+        return employeeList.stream()
+                .filter(x->x.getName().equals(name))
+                .sorted(Comparator.comparing(Employee::getSalary).reversed())
+                .toList();
+//        List<Employee> resultList = new ArrayList<>();
+//        for (var item : employeeList) {
+//            if (item.getName().equals(name)) {
+//                resultList.add(item);
+//            }
+//        }
+//        resultList.sort(Comparator.comparing(Employee::getSalary).reversed());
+//        return resultList;
     }
 
     public static Employee filterByNameAndFindWithMaxSalary(List<Employee> employeeList,
                                                             String name) {
-        List<Employee> resultList = new ArrayList<>();
-        for (var item : employeeList) {
-            if (item.getName().equals(name)) {
-                resultList.add(item);
-            }
-        }
-        return Collections.max(resultList, Comparator.comparing(Employee::getSalary));
+        return employeeList.stream()
+                .filter(x->x.getName().equals(name))
+                .max(Comparator.comparing(Employee::getSalary))
+                .orElseThrow();
+//        List<Employee> resultList = new ArrayList<>();
+//        for (var item : employeeList) {
+//            if (item.getName().equals(name)) {
+//                resultList.add(item);
+//            }
+//        }
+//        return Collections.max(resultList, Comparator.comparing(Employee::getSalary));
     }
 
 }
